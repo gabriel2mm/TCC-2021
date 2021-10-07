e61ca6db4efb9dc3b0672c4b1efba1c750a22cb2
Juliano Henrique <julianohenriquejhp@gmail.com>
2021-10-06 22:59:39 -0300

    Revert "Address não finlizado"
    This reverts commit a8c07af1669d881a6a5e660bf1296768c4ac5734.

--- Aplicação/Back-end/OCRFieldService/src/main/java/br/com/ocrfieldservice/dataprovider/dao/AddressDao.java
+++
@@ -1,9 +0,0 @@
-package br.com.ocrfieldservice.dataprovider.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.ocrfieldservice.core.entity.Address;
-
-public interface AddressDao extends JpaRepository<Address, Long> {
-
-}

926b003a0bbe4c2c4e54d2a6b78cdf1c5aa50d65
Juliano Henrique <julianohenriquejhp@gmail.com>
2021-10-06 23:06:18 -0300

    Revert "Revert "Address não finlizado""
    This reverts commit e61ca6db4efb9dc3b0672c4b1efba1c750a22cb2.

---
+++ Aplicação/Back-end/OCRFieldService/src/main/java/br/com/ocrfieldservice/core/entity/Address.java
@@ -0,0 +1,125 @@
+package br.com.ocrfieldservice.core.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ADDRESS")
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name="address")
+	private String address;
+	
+	@Column(name="district")
+	private String district;
+	
+	@Column(name="city")
+	private String city;
+	
+	@Column(name="state")
+	private String state;
+	
+	@Column(name="complement")
+	private String complement;
+	
+	@Column(name="number")
+	private String number;
+	
+	@Column(name="iat")
+	private Long iat;
+	
+	@Column(name="ing")
+	private Long ing;
+	
+	@Column(name="CEP")
+	private String CEP;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		id = id;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getComplement() {
+		return complement;
+	}
+
+	public void setComplement(String complement) {
+		this.complement = complement;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public Long getIat() {
+		return iat;
+	}
+
+	public void setIat(Long iat) {
+		this.iat = iat;
+	}
+
+	public Long getIng() {
+		return ing;
+	}
+
+	public void setIng(Long ing) {
+		this.ing = ing;
+	}
+
+	public String getCEP() {
+		return CEP;
+	}
+
+	public void setCEP(String CEP) {
+		this.CEP = CEP;
+	}
+	
+}
---
+++ Aplicação/Back-end/OCRFieldService/src/main/java/br/com/ocrfieldservice/core/usecase/AddressServiceImpl.java
@@ -0,0 +1,17 @@
+package br.com.ocrfieldservice.core.usecase;
+
+import org.springframework.stereotype.Service;
+
+import br.com.ocrfieldservice.contracts.AdressService;
+import br.com.ocrfieldservice.core.entity.Address;
+import br.com.ocrfieldservice.core.repository.AddressRepository;
+
+@Service
+public class AddressServiceImpl implements AdressService{
+	
+	@Override
+	public Address updateAddress(Long id, AddressRepository addressRepository) {
+		return AdressService.super.updateAddress(id, addressRepository);
+	}
+
+}
